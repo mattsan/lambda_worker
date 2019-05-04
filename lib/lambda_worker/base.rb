acf946ed0fbfb85844132d9426f1024961817d22
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-05-04 22:37:21 +0900

    add invoking functions

--- lib/lambda_worker/base.rb
+++ lib/lambda_worker/base.rb
@@ -1,26 +1,47 @@
 require 'aws-sdk-lambda'
+require 'active_support/core_ext/string'
 
 module LambdaWorker
   class Base
-    Config = Struct.new('Config', :aws_access_key_id, :aws_secret_access_key, :region, :profile)
+    Config = Struct.new('Config',
+                        :aws_access_key_id,
+                        :aws_secret_access_key,
+                        :region,
+                        :profile,
+                        :base_name,
+                        :stage)
 
     def self.configure
       yield config
     end
 
     def self.function(name)
-      define_singleton_method(name) do |args|
-        options = {
-          function_name: name,
-          invoke_args: args
-        }
+      define_singleton_method(name) do |**args|
+        client.invoke(
+          function_name: function_name(name),
+          payload: args.to_json
+        )
+      end
 
-        client.invoke_async(options)
+      define_singleton_method("#{name}_async") do |**args|
+        client.invoke_async(
+          function_name: function_name(name),
+          invoke_args: args.to_json
+        )
       end
     end
 
+    def self.function_name(name)
+      "#{config.base_name}-#{config.stage}-#{name}"
+    end
+
     def self.config
-      @config ||= Config.new
+      unless @config
+        @config = Config.new
+        @config.base_name = self.to_s.underscore.gsub('_', '-')
+        @config.stage = 'development'
+      end
+      @config
     end
 
     def self.client
